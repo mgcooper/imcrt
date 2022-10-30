2496cc5d2116c18b8a2870eb3612d85042643e5a
mgcooper <mguycooper@gmail.com>
2022-10-29 22:57:27 -0700

    added Setup.m

--- Setup.m
+++ Setup.m
@@ -1,11 +1,15 @@
-function Install()
+function Setup()
 % Setup.m setup the model paths etc.
 
 % add paths containing source code
 addpath(genpath([pwd() filesep 'src']));
 
-% remove paths containing example code 
-rmpath(genpath([pwd() filesep 'examples']));
+% remove paths containing example code
+try
+   rmpath([pwd() filesep 'examples']);
+catch
+end
+
 
 % display install message
 fprintf('\n * ice-Monte Carlo Radiative Transfer activated *\n\n')
